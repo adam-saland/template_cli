d947cbb0e0aa0568584b171a520c60124f900eb7
adam-saland <adam.saland@openfin.co>
2019-07-26 09:38:12 -0400

    create app-template is now fully functional

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const inquirer = require('inquirer');
-const {getRepoPath} = require('./lib/utils.js')
-const { launch, connect } = require('hadouken-js-adapter');
-const REACT_REPO_URL = 'https://github.com/adam-saland/openfin-react-devtools-demo.git'; const ANGULAR_REPO_URL = ''; const JS_REPO_URL = '';
-console.log(getRepoPath(REACT_REPO_URL))
-//console.log(getDirName('https://github.com/openfin/openfin-cli.git'))
-// Prompt user for the framework they would like to use
+const { setupTasks, banner, prompt } = require('./lib/cli.js');
+async function init() {
+    banner();
+    const app = await prompt()
+    const {framework} = app;
+    const frameworkDetails = Object.assign({}, ...framework);
+    setupTasks(frameworkDetails).then(console.log)
+
+    
+    console.log(frameworkDetails)
+}
+
+init()
